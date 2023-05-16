e41d58866bf383f6cf6ccc7f2bb8ad9c471a8217
luankofs <luan.luan2009@hotmail.com>
2023-05-16 20:53:49 -0300

    Gallery e secure js

--- js/script.js
+++ js/script.js
@@ -21,3 +21,52 @@ parametros.forEach(function (parametro) {
     element.checked = "true";
   }
 });
+
+/* ABRIR/ FECHAR PERGUNTAS FREQUENTES */
+
+// const getQuestion = document.querySelectorAll(".perguntas dl dt");
+
+// getQuestion.forEach(function (question) {
+//   question.addEventListener("click", showAnswer);
+// });
+
+// function showAnswer() {
+//   const show = this.nextElementSibling;
+//   console.log(show);
+//   if (show) {
+//     show.classList.toggle("perg-Active");
+//   }
+// }
+
+const perguntas = document.querySelectorAll(".perguntas button");
+perguntas.forEach(function (pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+});
+function ativarPergunta(event) {
+  //pegar o evento de clique no button
+  const pergunta = event.currentTarget;
+  //pegar o aria-controls que irá ser usado como id do button capturado pelo clique
+  const controls = pergunta.getAttribute("aria-controls");
+  //pegar o elemento dd pelo id.
+  const resposta = document.getElementById(controls);
+  resposta.classList.toggle("ativa");
+  //verificar se resposta contém ativa, se conter é ativa, e seta com setAttribute.
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+// galeria de imagens //
+
+const gallery = document.querySelectorAll(".bicicleta-img img");
+const galleryContainer = document.querySelector(".bicicleta-img");
+function eventsGallery(img) {
+  img.addEventListener("click", switchImg);
+}
+function switchImg(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galleryContainer.prepend(img);
+  }
+}
+gallery.forEach(eventsGallery);
